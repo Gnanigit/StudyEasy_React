40ddfda1bc7469934b23aaecc8476e0c5505f8e3
Gnanigit <gnani4412@gmail.com>
2024-05-20 16:46:53 +0530

    added like facility

--- server/server.js
+++ server/server.js
@@ -9,13 +9,13 @@ const app=express();
 const port=8080
 
 
-// const allowedOrigin = 'https://study-easy-react.vercel.app';
+const allowedOrigin = 'https://study-easy-react.vercel.app';
 // uses
 app.use(express.json({ limit: '100mb' }));
-// app.use(cors({
-//     origin: allowedOrigin
-//   }));
-app.use(cors())
+app.use(cors({
+    origin: allowedOrigin
+  }));
+// app.use(cors())
 
 app.use(morgan('tiny'))
 app.disable('x-powered-by')    // less hackers know about our stack
